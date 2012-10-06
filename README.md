bda314b86bfc09e673619f6a98ba6398f7291ff4
Hiroshi Yoshida <hrysd22@gmail.com>
2012-10-06 18:39:27 +0900

    トゥワンがトゥー

---
+++ README.md
@@ -0,0 +1,8 @@
+# MISAWA::MD
+
+Githubでのコードレビューを濃厚にする拡張です。
+
+[詳細](http)
+
+## Install
+
---
+++ src/javascripts/popup.js
@@ -0,0 +1,19 @@
+function copy(text) {
+  var clip = document.getElementById('tmp_area');
+  clip.value = text;
+  clip.select();
+  document.execCommand('copy');
+}
+
+$(function() {
+  $.getJSON('data.json', function(data) {
+    var images;
+    images = data;
+    $.each(data, function(key, val) {
+      $('#images').append("<img class='image" + key + "' src='" + val['image'] + "' />");
+      $('.image' + key).click(function(){
+        copy("![みさわ](" + $(this).attr('src') + ")");
+      })
+    });
+  });
+});
